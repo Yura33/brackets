2d2672e087bdf5fff695a04b0f58bd558cb16e5a
Yury Kovaliov <yurykavaliou86@gmail.com>
2020-10-25 13:23:50 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let sample = [];
+  bracketsConfig.forEach(el => sample.push(el[0].concat(el[1])));;
+  let temp = str;
+  let a;
+  for (let i = 0; i < sample.length; i++) {
+      let b = sample[i];
+      a = temp.replace(b, '');
+      temp = a;
+  }
+
+  if (temp === str) { 
+    return false;
+  } else if (temp === '') { 
+    return true;
+  } else return check(temp, bracketsConfig); 
+  
 }
